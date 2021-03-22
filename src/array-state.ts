b22b87d99f97086ad35253a7512879d792b9bf92
Vyacheslav Sotsenko <Vyacheslav_Sotsenko@epam.com>
2021-03-22 14:22:49 +0200

    (refactor) array-state

--- src/array-state.ts
+++ src/array-state.ts
@@ -1,44 +1,40 @@
 import { BaseState } from './base-state';
 
 export abstract class ArrayState<T> extends BaseState<T[]> {
-
-
 	public set(array: T[]): void {
 		this.setNewValue(array);
 	}
 
 	public getByIndex(index: number): T {
 		try {
-			const items: T[] = this.data as T[];
+			const items = this.data as T[];
 
 			return items[index];
-		} catch(e) {
-			throw this.getErrorMessage(e, 'get by index');
+		} catch (error) {
+			throw this.getErrorMessage(error, 'get by index');
 		}
 	}
 
-	public addItem (item: T): void  {
+	public addItem(item: T): void  {
 		try {
-			const items: T[] = this.data as T[];
+			const items = this.data as T[];
 
 			items.push(item);
 
 			this.setNewValue(items);
-		} catch(e) {
-			throw this.getErrorMessage(e, 'add item');
+		} catch (error) {
+			throw this.getErrorMessage(error, 'add item');
 		}
 	}
 
 	public removeItem(item: T): void {
 		try {
-			const items: T[] = (this.data as T[])
-				.filter(_currentItem => 
-					!this.compareItems(_currentItem, item)
-				);
+			const items = (this.data as T[])
+				.filter((currentItem) => !this.compareItems(currentItem, item));
 
 			this.setNewValue(items);
-		} catch(e) {
-			throw this.getErrorMessage(e, 'remove item');
+		} catch (error) {
+			throw this.getErrorMessage(error, 'remove item');
 		}
 	}
 
@@ -49,8 +45,8 @@ export abstract class ArrayState<T> extends BaseState<T[]> {
 			items[index] = itemToChange;
 
 			this.setNewValue(items);
-		} catch(e) {
-			throw this.getErrorMessage(e, 'change item by index');
+		} catch (error) {
+			throw this.getErrorMessage(error, 'change item by index');
 		}
 	}
 
@@ -58,15 +54,13 @@ export abstract class ArrayState<T> extends BaseState<T[]> {
 		try {
 			const items = this.data as T[];
 			const itemIndex = (items)
-				.findIndex(_currentItem => 
-					this.compareItems(_currentItem, itemToUpdate)
-				);
+				.findIndex((currentItem) => this.compareItems(currentItem, itemToUpdate));
 
 			items[itemIndex] = itemToUpdate;
 
 			this.setNewValue(items);
-		} catch(e) {
-			throw this.getErrorMessage(e, 'update item');
+		} catch (error) {
+			throw this.getErrorMessage(error, 'update item');
 		}
 	}
 
