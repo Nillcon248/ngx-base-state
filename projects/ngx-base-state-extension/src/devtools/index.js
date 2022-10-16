5486c630a23d3f88ba29f2174ecc0c2de90800a1
DreyLiky <dreyliky@gmail.com>
2022-10-16 13:33:21 +0300

    (feature) icon

--- projects/ngx-base-state-extension/src/devtools/index.js
+++ projects/ngx-base-state-extension/src/devtools/index.js
@@ -1,4 +1,5 @@
-const appEntryHtmlFilePath = '../index.html';
+const appEntryHtmlFilePath = '/index.html';
+const iconPath = '/assets/icons/icon-48.png';
 const panelName = 'ngx-base-state';
 
-chrome.devtools.panels.create(panelName, null, appEntryHtmlFilePath);
+chrome.devtools.panels.create(panelName, iconPath, appEntryHtmlFilePath);
