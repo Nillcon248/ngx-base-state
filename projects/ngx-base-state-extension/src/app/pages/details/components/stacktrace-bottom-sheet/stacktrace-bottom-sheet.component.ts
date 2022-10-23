b8ba2c57fbae67ca283fa96029f865f3d6663844
DreyLiky <dreyliky@gmail.com>
2022-10-23 16:09:56 +0300

    (fix) extension

--- projects/ngx-base-state-extension/src/app/pages/details/components/stacktrace-bottom-sheet/stacktrace-bottom-sheet.component.ts
+++ projects/ngx-base-state-extension/src/app/pages/details/components/stacktrace-bottom-sheet/stacktrace-bottom-sheet.component.ts
@@ -1,6 +1,9 @@
-import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
-import { MAT_BOTTOM_SHEET_DATA } from '@angular/material/bottom-sheet';
+import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
+import { MatBottomSheetRef, MAT_BOTTOM_SHEET_DATA } from '@angular/material/bottom-sheet';
+import { Subscription } from 'rxjs';
+import { ApplicationReloadService } from '@extension-services';
 import { ɵMetadataOperation } from '@ngx-base-state';
+import { AutoUnsubscribe } from 'ngx-auto-unsubscribe-decorator';
 
 @Component({
     selector: 'app-stacktrace-bottom-sheet',
@@ -8,8 +11,20 @@ import { ɵMetadataOperation } from '@ngx-base-state';
     styleUrls: ['./stacktrace-bottom-sheet.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class StacktraceBottomSheetComponent {
+export class StacktraceBottomSheetComponent implements OnInit {
     constructor(
-        @Inject(MAT_BOTTOM_SHEET_DATA) public readonly operation: ɵMetadataOperation
+        @Inject(MAT_BOTTOM_SHEET_DATA) public readonly operation: ɵMetadataOperation,
+        private readonly applicationReloadService: ApplicationReloadService,
+        private readonly bottomSheetRef: MatBottomSheetRef
     ) {}
+
+    public ngOnInit(): void {
+        this.initApplicationReloadObserver();
+    }
+
+    @AutoUnsubscribe()
+    private initApplicationReloadObserver(): Subscription {
+        return this.applicationReloadService.onReload$
+            .subscribe(() => this.bottomSheetRef.dismiss());
+    }
 }
--- projects/ngx-base-state/src/lib/helpers/stack-trace.helper.ts
+++ projects/ngx-base-state/src/lib/helpers/stack-trace.helper.ts
@@ -1,13 +1,11 @@
 export abstract class ɵStackTrace {
     public static capture(): string[] {
         // FIXME: Refactor and investigate
-        const amountOfInternalMethodCall = 4;
         const stackTrace = new Error().stack!;
         const rawMethods = stackTrace.split('at ');
         const methods = rawMethods
             .map((rawMethod) => rawMethod.slice(0, rawMethod.indexOf(' (')))
-            .filter((method) => !method.startsWith('http'))
-            .slice(amountOfInternalMethodCall);
+            .filter((method) => !method.startsWith('http'));
 
         return methods;
     }
