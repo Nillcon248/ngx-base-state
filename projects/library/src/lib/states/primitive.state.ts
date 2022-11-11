4beade68c0148c14ba6ca8114b75fd7acf483592
ikachkov <usemuse@mail.ru>
2022-11-12 01:06:42 +0300

    PrimitiveState: added Generic Constraints

--- projects/library/src/lib/states/primitive.state.ts
+++ projects/library/src/lib/states/primitive.state.ts
@@ -4,4 +4,6 @@ import { BaseState } from './base.state';
  *	@class
  *	@classdes Primitive state class. Used for save state with Primitive type (Like boolean, number or string).
  */
-export class PrimitiveState<T> extends BaseState<T> {}
+export class PrimitiveState<
+  T = string | number | boolean | bigint | symbol
+> extends BaseState<T> {}
